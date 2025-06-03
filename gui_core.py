32992f75471d9cf1b07558e1acc77a3a961acd54
Milade <deminetakinkuotu@gmail.com>
2025-06-03 19:40:34 +0100

    commit 07/40

--- gui_core.py
+++ gui_core.py
@@ -5,7 +5,7 @@ import threading
 import keyboard
 from cryptography.fernet import Fernet
 
-from PY_flow import hash_text, decrypt, encrypt
+from PY_flow import hash_text, decrypt
 
 key = b'62yvjjRaK0zdh_qg69vV6ULeNCXr-ieD1Z5P_7emj0M='
 cipher = Fernet(key)
@@ -19,33 +19,30 @@ class GuiCore:
         specific_copy = input("specfic copy: ")
         conn_a = sqlite3.connect('data_base.db')
         crsr_a = conn_a.cursor()
-        crsr_a.execute('''SELECT * FROM copy_data''')
-        rows_a = crsr_a.fetchall()
-        for row_a in rows_a:
-            check = decrypt(row_a[1])
-            if check == specific_copy:
-                return "check"
-            elif check != specific_copy:
-                return f"{specific_copy} a not found in Data-Base"
-            else:
-                return "NULL"
+        check = hash_text(specific_copy)
+        crsr_a.execute('''SELECT * FROM copy_data WHERE hash = ?''', (check,))
+        found = crsr_a.rowcount
+        if found > 0:
+            print("gone")
+        else:
+            print(f"{specific_copy} not in database")
+        conn_a.close()
+
+
     @staticmethod
     def specific_paste_pin():
         specific_copy = input("specfic copy: ")
         conn_a = sqlite3.connect('data_base.db')
         crsr_a = conn_a.cursor()
-        crsr_a.execute('''SELECT * FROM pin_data''')
-        rows_a = crsr_a.fetchall()
-        found = False
-        for row_a in rows_a:
-            check = decrypt(row_a[1])
-            if check == specific_copy:
-                 found = True
-                 return check, row_a[2]
-        if found:
+        check = hash_text(specific_copy)
+        crsr_a.execute('''SELECT * FROM pin_data WHERE hash = ?''', (check,))
+        found = crsr_a.rowcount
+        conn_a.commit()
+        if found > 0:
             print("gone")
         else:
             print(f"{specific_copy} not in database")
+        conn_a.close()
 
     @staticmethod
     def delete_copy():
@@ -60,6 +57,7 @@ class GuiCore:
             print("gone")
         else:
             print(f"{specific_copy} not in database")
+        conn_a.close()
 
 
 
@@ -76,6 +74,7 @@ class GuiCore:
             print("gone")
         else:
             print(f"{specific_copy} not in database")
+        conn_a.close()
 
     @staticmethod
     def show_copy_history():
@@ -87,6 +86,7 @@ class GuiCore:
         for row in rows:
             data = decrypt(row[0])
             print(f"kk{data} - {row[1]}")
+        conn.close()
 
     @staticmethod
     def pin_history():
@@ -98,6 +98,7 @@ class GuiCore:
         for row in rows:
             data = decrypt(row[0])
             print(f"ff{data} - {row[1]}")
+        conn.close()
 
 
 
@@ -119,8 +120,5 @@ def threaded_pin_history():
 
 
 if __name__ == '__main__':
-    #keyboard.add_hotkey('ctrl+alt+u', threaded_specific_paste)  # Paste specific copied content
-    #keyboard.add_hotkey('ctrl+alt+r', threaded_specific_pin_paste)  # Paste specific pinned content
-    r = GuiCore()
-    r.show_copy_history()
-    print(r.delete_copy())
+    keyboard.add_hotkey('ctrl+alt+u', threaded_specific_paste)  # Paste specific copied content
+    keyboard.add_hotkey('ctrl+alt+r', threaded_specific_pin_paste)  # Paste specific pinned content
